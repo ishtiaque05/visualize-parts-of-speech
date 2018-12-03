97932fab3c73c69c3c56fb2c03911cb4c7df20c4
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2018-12-03 16:49:58 +0600

    Generate json data for visualization

--- createPosNetwork.py
+++ createPosNetwork.py
@@ -1,9 +1,9 @@
 import nltk
-from nltk.corpus import state_union
-from nltk.tokenize import PunktSentenceTokenizer
+import json
 import tkinter as tk
+from nltk.corpus import state_union
+from nltk.tokenize import PunktSentenceTokenizer, word_tokenize
 from tkinter import filedialog
-
 
 inputStr = input("[y]Open custom file or [n]use sample text for visualization? [y/n]")
 inputStr = inputStr.lower()
@@ -16,17 +16,36 @@ if(inputStr == 'y' or inputStr == 'yes'):
 else:
 	sample_text = state_union.raw("2006-GWBush.txt")
 
-print(sample_text)
 
-def process_content():
-    try:
-        for i in tokenized[:5]:
-            words = nltk.word_tokenize(i)
-            tagged = nltk.pos_tag(words)
-            print(tagged)
+def create_json(word_freq):
+	pos = ["CC","CD","DT","EX","FW", "IN", 
+			"JJ", "JJR","JJS","LS", "MD",
+			"NN", "NNS", "NNP", "NNPS", "PDT",
+			"POS","PRP","PRP$", "RB","RBR",
+			"RBS","RP","TO","UH", "VB"
+			"VBD", "VBG", "VBN", "VBP", "VBZ",
+			"WDT", "WP", "WP$", "WRD"
+		]
+	data = {}
+	nodes = []
+	links = []			
+	
+	for word, freq in word_freq.items():
+		nodes.append({"id": word, "group": "words"})
+		links.append( {"source": word, "target": nltk.tag.pos_tag([word])[0][1], "value": freq})
 
-    except Exception as e:
-        print(str(e))
+	for tag in pos:
+		nodes.append({"id": tag, "group": "pos"})
+
+	data['nodes'] = nodes
+	data['links'] = links
+
+	with open('data.json', 'w') as outfile:
+		json.dump(data, outfile)
+		
+tokenized = word_tokenize(sample_text)
+word_frequency_dist = nltk.FreqDist(tokenized)
 
 
-#process_content()
+word_freq = dict((tokenized, freq) for tokenized, freq in word_frequency_dist.items())
+create_json(word_freq)
